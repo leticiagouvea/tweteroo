6f180bde512696612775a2d62f5ccc8b7c64dacd
leticiagouvea <letfgouvea@gmail.com>
2022-11-07 13:57:22 -0300

    POST request: sign-up and tweets

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const users = [];
+const tweets = [];
+
+app.post("/sign-up", (req, res) => {
+    const { username, avatar } = req.body;
+    users.push({ username, avatar });
+    res.send("OK");
+});
+
+app.post("/tweets", (req, res) => {
+    const { username, tweet } = req.body;
+    const { avatar } = users.find(user => user.username === username);
+
+    tweets.push({ username, tweet, avatar });
+    res.send("OK");
+});
+
 app.listen(5000, () => {
     console.log("App is running in port: 5000")
 });
